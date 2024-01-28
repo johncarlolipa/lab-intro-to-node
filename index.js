845af6a2a3d5646e5fcdee2953fed44a3b195260
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-28 18:53:58 +0800

    setup add item

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+ 
 }
 
 module.exports = SortedList;
